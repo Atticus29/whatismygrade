3307fd0bd55f4b5d3dee89d02d4c1a0a2d3c51ca
Mark <mark.aaron.fisher@gmail.com>
2018-03-08 22:17:49 -0800

    freeze with incomplete progress on the table front

--- src/app/d3.service.ts
+++ src/app/d3.service.ts
@@ -3,8 +3,6 @@ import * as d3 from 'd3';
 
 @Injectable()
 export class D3Service {
-  private tableData = [];
-
   constructor() { }
 
   loadTable() {
@@ -13,31 +11,38 @@ export class D3Service {
     var table = d3.select('#hi')
       .append('table')
       .classed('table', true);
-
     var thead = table.append('thead').append('tr');
-
     var tbody = table.append('tbody');
 
     var reload = function(){
-      d3.csv("http://localhost:4200/assets/master_grades_lecture.csv", function(error, rows){
-        data = rows;
-        console.log(data);
+      d3.csv("http://localhost:4200/assets/master_grades_lecture.csv", function(error, entries){
+        data = entries;
         redraw(data);
       });
     }
 
     var redraw = function(data){
+      console.log(data.length);
+
       var keys = d3.map(data[0]).keys()
       thead.selectAll("th")
         .data(keys)
         .enter()
         .append('th')
         .text(function(d) {return d;});
-      console.log("redraw called");
+
+      var rows = tbody.selectAll("tr")
+        .data(data);
+      console.log(d3.map(data).values());
+      rows.enter().append('tr');
+      rows.exit().remove();
+      var cells = rows.selectAll('td')
+        .data(function(data) {return d3.map(data).values();});
+      cells.enter().append('td');
+      cells.text(function(d){return d;});
     }
 
     reload();
-
   }
 
 }
